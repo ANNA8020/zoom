dab741a699ceeed5ad661ccbf7c8b59a5c98419a
ANNA8020 <limJE2005@gmail.com>
2021-10-12 12:08:13 +0900

    유저의 카메라 획득(?)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,19 +3,36 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 // stream: 비디오 + 오디오
 let muted = false;
 let cameraOff = false;
 
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 async function getMedia() {
     try {
         myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
             video: true,
+            audio: true,
         });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch (e) {
         console.log(e);
     }
@@ -35,7 +52,7 @@ function handleMuteClick() {
 }
 
 function handleCameraClick(){
-    console.log(myStream.getVideoTracks());
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
     if (cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
