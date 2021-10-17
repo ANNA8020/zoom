e325061fb970c4780db3da141c904440d10b4a3e
ANNA8020 <limJE2005@gmail.com>
2021-10-17 21:50:52 +0900

    data-channel

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,6 +16,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
     try {
@@ -123,6 +124,9 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // socket code
 
 socket.on("welcome", async () => {
+    myDataChannel = myPeerConnection.createDataChannel("chat");
+    myDataChannel.addEventListener("message", (event) => console.log(event.data));
+    console.log("made data channel");
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
     console.log("Sent The Offer");
@@ -131,6 +135,10 @@ socket.on("welcome", async () => {
 // 브라우저 1
 
 socket.on("offer", async (offer) => {
+    myPeerConnection.addEventListener("datachannel", (event) => {
+        myDataChannel = event.channel;
+        myDataChannel.addEventListener("message", (event) => console.log(event.data));
+    });
     console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
     const answer = await myPeerConnection.createAnswer();
